2a9833607632430898c882f3aa5d6667f2c29a7d
Kamaropoulos <kamaropoulos@outlook.com>
2017-08-14 15:27:29 +0300

    Added random encryption function selection script

---
+++ encryption.py
@@ -0,0 +1,96 @@
+import string
+import random
+
+from Crypto.Hash import *
+from Crypto.Cipher import *
+from Crypto import Random
+
+def random_string(length, characters=string.ascii_uppercase + string.ascii_lowercase + string.digits):
+	return ''.join(random.choice(characters) for _ in range(length))
+
+def Hash_HMAC(string):
+	secret = random_string(5)
+	h = HMAC.new(secret)
+	h.update(string)
+	return [h.hexdigest(), ""]
+
+def Hash_MD2(string):
+	h = MD2.new()
+	h.update(string)
+	return h.hexdigest()
+
+def Hash_MD4(string):
+	h = MD4.new()
+	h.update(string)
+	return h.hexdigest()
+
+def Hash_MD5(string):
+	h = MD5.new()
+	h.update(string)
+	return h.hexdigest()
+
+def Hash_RIPEMD(string):
+	h = RIPEMD.new()
+	h.update(string)
+	return h.hexdigest()
+
+def Hash_SHA(string):
+	h = SHA.new()
+	h.update(string)
+	return h.hexdigest()
+
+def Hash_SHA224(string):
+	h = SHA224.new()
+	h.update(string)
+	return h.hexdigest()
+
+def Hash_SHA256(string):
+	h = SHA256.new()
+	h.update(string)
+	return h.hexdigest()
+
+def Hash_SHA384(string):
+	h = SHA384.new()
+	h.update(string)
+	return h.hexdigest()
+
+def Hash_SHA512(string):
+	h = SHA512.new()
+	h.update(string)
+	return h.hexdigest()
+
+#def Cipher_AES(string):
+#	key = random_string(16)
+#	iv = Random.new().read(AES.block_size)
+#	cipher = AES.new(key, AES.MODE_CFB, iv)
+#	msg = iv + cipher.encrypt(string)
+#	return msg
+
+#def Cipher_ARC2(string):
+#	 key = random_string(16)
+#	iv = Random.new().read(ARC2.block_size)
+#	cipher = ARC2.new(key, ARC2.MODE_CFB, iv)
+#	msg = iv + cipher.encrypt(string)
+#	return msg
+
+def Cipher_ARC4(string):
+	key = random_string(32)
+	nonce = Random.new().read(16)
+	tempkey = SHA.new(key+nonce).digest()
+	cipher = ARC4.new(tempkey)
+	msg = nonce + cipher.encrypt(string)
+	return msg
+
+def Cipher_Caesar(plaintest):
+	shift = randint(1,26)
+	alphabet = string.ascii_lowercase
+	shifted_alphabet = alphabet[shift:] + alphabet[:shift]
+	table = string.maketrans(alphabet, shifted_alphabet)
+	return plaintext.translate(table)
+
+if __name__ == '__main__':
+	functions_list = [Hash_HMAC, Hash_MD2, Hash_MD4, Hash_MD5, Hash_RIPEMD, Hash_SHA, Hash_SHA224, Hash_SHA256, Hash_SHA384, Hash_SHA512, Cipher_ARC4, Cipher_Caesar]
+	function_to_call = random.choice(functions_list)
+	result = function_to_call("Hello")
+	print "Function: " + function_to_call.__name__
+	print "Result: " + result
